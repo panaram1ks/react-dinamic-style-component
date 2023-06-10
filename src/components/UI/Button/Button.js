b7da068e57f6720e77828cb62554af2064aca4a6
evgen <parom.evgen@mail.ru>
2023-06-10 12:04:06 +0300

    use and instal Style-Components lib

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,11 +1,33 @@
-import "./Button.css";
+/* import "./Button.css"; */
+import styled from "styled-components";
 
-const Button = (props) => {
+/* const Button = (props) => {
   return (
     <button type={props.type} className="button">
       {props.children}
     </button>
   );
-};
+}; */
+
+//Tagged template literals
+const Button = styled.button`
+  font: inherit;
+  padding: 0.5rem 1.5rem;
+  color: white;
+  background: #00358b;
+  border-radius: 4px;
+  box-shadow: 0 0 4px rgba(50, 50, 50, 0.25);
+  cursor: pointer;
+
+  &:focus {
+    outline: none;
+  }
+
+  &:hover,
+  &:active {
+    background: #245fbd;
+    box-shadow: 0 0 8px rgba(50, 50, 50, 0.25);
+  }
+`;
 
 export default Button;
