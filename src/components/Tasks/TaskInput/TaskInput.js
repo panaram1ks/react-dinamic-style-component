2cf7bfe6ec9faf34c4bd5a95aee8eee125485e72
evgen <parom.evgen@mail.ru>
2023-06-10 11:38:06 +0300

    inline Style has General Priority

--- src/components/Tasks/TaskInput/TaskInput.js
+++ src/components/Tasks/TaskInput/TaskInput.js
@@ -6,20 +6,36 @@ import "./TaskInput.css";
 const TaskInput = (props) => {
   const [inputText, setinputText] = useState("");
 
+  const [isInputValid, setIsInputValue] = useState(true);
+
   const taskInputChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setIsInputValue(true);
+    }
     setinputText(event.target.value);
   };
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
+    if (inputText.trim().length === 0) {
+      setIsInputValue(false);
+      return;
+    }
     props.onAddTask(inputText);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Задачи</label>
-        <input type="text" onChange={taskInputChangeHandler} />
+        <label style={{ color: !isInputValid ? "red" : "black" }}>Задачи</label>
+        <input
+          style={{
+            borderColor: !isInputValid ? "red" : "black",
+            background: !isInputValid ? "salmon" : "transparent",
+          }}
+          type="text"
+          onChange={taskInputChangeHandler}
+        />
       </div>
       <Button type="submit">Добавить Задачу</Button>
     </form>
