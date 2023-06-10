55c509b43157351e7ad2b69f1198532fe6bfc4c2
evgen <parom.evgen@mail.ru>
2023-06-10 12:53:21 +0300

    change style use style-components lib dinamicly

--- src/components/Tasks/TaskInput/TaskInput.js
+++ src/components/Tasks/TaskInput/TaskInput.js
@@ -1,7 +1,42 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 
 import Button from "../../UI/Button/Button";
 import "./TaskInput.css";
+
+const FormControl = styled.div`
+  margin: 0.5rem 0;
+
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid #ccc;
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  & input:focus {
+    outline: none;
+    background: #c8e1e4;
+    border-color: #00358b;
+  }
+
+  &.invalid input {
+    border-color: red;
+    background: #d087df;
+  }
+
+  &.invalid label {
+    color: red;
+  }
+`;
 
 const TaskInput = (props) => {
   const [inputText, setinputText] = useState("");
@@ -26,10 +61,12 @@ const TaskInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${!isInputValid ? "invalid" : ""}`}>
+      {/* <div className={`form-control ${!isInputValid ? "invalid" : ""}`}> */}
+      <FormControl className={!isInputValid && "invalid"}>
         <label>Задачи</label>
         <input type="text" onChange={taskInputChangeHandler} />
-      </div>
+        {/* </div> */}
+      </FormControl>
       <Button type="submit">Добавить Задачу</Button>
     </form>
   );
