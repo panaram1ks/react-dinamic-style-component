8f234e03fd958e5306d16efcb2da237960d446df
evgen <parom.evgen@mail.ru>
2023-06-10 13:12:51 +0300

    use media query in style-component lib

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -11,6 +11,7 @@ import styled from "styled-components";
 
 //Tagged template literals
 const Button = styled.button`
+  width: 100%;
   font: inherit;
   padding: 0.5rem 1.5rem;
   color: white;
@@ -18,6 +19,10 @@ const Button = styled.button`
   border-radius: 4px;
   box-shadow: 0 0 4px rgba(50, 50, 50, 0.25);
   cursor: pointer;
+
+  @media (min-width: 700px) {
+    width: auto;
+  }
 
   &:focus {
     outline: none;
